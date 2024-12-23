10653a65f9ddedb135097440f21760f113256ab3
tuanpa2295 <tuanpa2295@gmail.com>
2024-12-23 19:52:43 +0700

    Add orders list

---
+++ app/models/Order.server.ts
@@ -0,0 +1,48 @@
+import type { Order, Prisma} from '@prisma/client';
+
+export async function createOrder(data: Omit<Order, "id" | "createdAt">) {
+  return await prisma.order.create({ data });
+}
+
+export const createOrUpdate = async (orderData: Prisma.OrderWhereInput) => {
+  const { orderId, ...data } = orderData;
+  try {
+    const existingUser = await prisma.order.findFirst({
+      where: { orderId },
+    })
+
+    if (existingUser) {
+      // If user exists, update the email
+      return await prisma.order.update({ where: { orderId: orderId as string }, data: data as Prisma.OrderUpdateInput })
+    } else {
+      // If user doesn't exist, create a new user
+      return await prisma.order.create({ data: orderData as Prisma.OrderCreateInput })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function getOrderById(id: number) {
+  return await prisma.order.findUnique({ where: { id } });
+}
+
+export async function updateOrder(id: number, data: Partial<Order>) {
+  return await prisma.order.update({
+    where: { id },
+    data,
+  });
+}
+
+export async function deleteOrder(id: number) {
+  return await prisma.order.delete({ where: { id } });
+}
+
+export async function getOrders() {
+  const orders = await prisma.order.findMany({
+    orderBy: { id: "desc" },
+    where: {}
+  });
+
+  return orders;
+}
--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -24,7 +24,10 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/orders" rel="orders">
+          Orders
+        </Link>
+        {/* <Link to="/app/additional">Additional page</Link> */}
       </NavMenu>
       <Outlet />
     </AppProvider>
